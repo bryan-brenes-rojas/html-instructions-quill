5a7de70d788510b3ad41886e2b3dc53d7943f0fd
Bryan Brenes <bryanbrenes1@gmail.com>
2022-08-15 10:38:36 -0600

    Add ngx-quill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { QuillModule } from 'ngx-quill';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InstructionEditorComponent } from './instruction-editor/instruction-editor.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, InstructionEditorComponent],
+  imports: [BrowserModule, AppRoutingModule, QuillModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/instruction-editor/instruction-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instruction-editor',
+  templateUrl: './instruction-editor.component.html',
+  styleUrls: ['./instruction-editor.component.scss'],
+})
+export class InstructionEditorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
